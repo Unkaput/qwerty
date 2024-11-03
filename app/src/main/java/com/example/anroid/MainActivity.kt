ba8bdd9db6a79a5e98ac4cc28a5fbbc1529c69c1
unknown <mkm082005@gmail.com>
2024-11-03 15:48:41 +0300

    ffffff

---
+++ app/src/main/java/com/example/anroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.anroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+
+}
+
+
